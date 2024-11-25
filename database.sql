97154e019924dc6ed33b7d8ccff203ee7e4752ac
Shobha Chejara <shobha.chejara@gmail.com>
2024-11-25 14:08:02 -0600

    updated dabase file

--- database.sql
+++ database.sql
@@ -3,7 +3,13 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    id SERIAL PRIMARY KEY UNIQUE,
+    email TEXT UNIQUE,
+    first_name TEXT,
+    last_name TEXT,
+    password TEXT,
+    dayEndTime TIME,
+    notify BOOLEAN,
+    challenge_complete BOOLEAN,
 );
+
