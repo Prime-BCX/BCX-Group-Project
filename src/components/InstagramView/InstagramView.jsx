eea935f0f0b99d35f24d00a577d3ded6996b9bf9
Tom Pinataro <tompinataro@gmail.com>
2024-11-26 12:48:26 -0600

    Instagram Started

---
+++ src/components/InstagramView/InstagramView.jsx
@@ -0,0 +1,17 @@
+// This view promotes the use of Instagram to share the user's progress.
+import image from '.../images/BCX_and_Instagram.jpeg';
+
+<container>
+<h2>
+    Easily share you progress to
+    Instagram Stories
+    straight from the app!
+</h2>
+
+<img src={BCX_and_Instagram.jpeg} alt="BCX and Instagram" />
+
+
+
+</container>
+
+
