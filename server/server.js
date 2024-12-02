bc1547522c17eba0bf96e27d63521df6dab368d4
Jamie McCullough <92225779+KeeganMcCullough@users.noreply.github.com>
2024-12-02 09:32:55 -0600

    cronjob testing

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cron = require('node-cron');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 5001;
@@ -24,6 +25,20 @@ app.use(passport.session());
 
 // Routes
 app.use('/api/user', userRouter);
+
+cron.schedule('0 * * * *', () => {
+  console.log('running hourly job for', Date.now);
+  /*
+select "user".id, email, first_name, step, daily_hydrate, daily_grow, daily_move, daily_focus, daily_nourish,daily_dinner
+from "user" 
+join userProgress on "user".id = userProgress.id 
+join dailyHabits on "user".id = dailyHabits.user_id 
+where 
+"dayEndTime" = '2:00:00' and 
+notify = true and 
+challenge_complete = false
+*/
+})
 
 // Listen Server & Port
 app.listen(PORT, () => {
