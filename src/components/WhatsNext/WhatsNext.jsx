65b5f0e09f2c2dba286737a7686bccfbf0469dc6
Tom Pinataro <tompinataro@gmail.com>
2024-12-06 10:27:19 -0600

    Improve Buttons on RepeatOrRestart and WhatsNext

--- src/components/WhatsNext/WhatsNext.jsx
+++ src/components/WhatsNext/WhatsNext.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import VideoCreator from "../VideoCreator/VideoCreator";
 import './WhatsNext.css';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import ButtonToGo from "../ButtonToGo/ButtonToGo";
 
 
 
@@ -22,25 +23,7 @@ function WhatsNext() {
             <div className="bottom">
                 <h2 className="style">WHAT'S NEXT?</h2>
 
-
-
-
-
-                <ButtonToGo label="Repeat Phase One" path={`/StepStarterVideo/${id}`}   />
-                <ButtonToGo label="Repeat Phase Two" path={`/StepStarterVideo/${id}`}   />
-
-                {/* <button
-                    className="btn"
-                    onClick={ }>Repeat Phase One
-                </button>
-
-                <button onClick={ }>Restart Phase Two
-
-                </button> */}
-
-                <button>
-                I COMPLETED BCX
-                </button>
+                <ButtonToGo label="I COMPLETED BCX!" path="/RepeatOrRestart" />
 
             </div>
 
