61d5c44349984d9ee00a1ed04a20259f0245be21
Tom Pinataro <tompinataro@gmail.com>
2024-12-06 18:41:30 -0600

    Make improvements to missedDay

--- src/components/MissedDayView/MissedDayView.jsx
+++ src/components/MissedDayView/MissedDayView.jsx
@@ -1,25 +1,25 @@
 import ButtonToGo from '../ButtonToGo/ButtonToGo';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import "./MissedDayView.css";
+
 function MissedDayView() {
     const id = useParams;
 
     return (
-        <div className="container">
+        <div>
+        <div className='background'>
+            <div className="Missed-Container">
 
-            <h2> What Happened? </h2>
-            <p> Let's check in.  Did you complete today's steps but forget to submit your photo, or is it time to reset and rebuild?
-            </p>
-
-            {/* <img src="../Images/MissedDayImage.jpeg" alt="An illustration of a missed day" /> */}
-           
-            {/* <div className="background">
-                <h1 style={{ color: 'white', textAlign: 'center', paddingTop: '20px' }}>
-                </h1>
-            </div> */}
+                <h2 id="WH"> WHAT HAPPENED? </h2>
+               
+                <p id="PWH" >Let's check in. Did you complete today's steps but forget to submit your photo, 
+                    or is it time to reset and rebuild?
+                </p>
             
-            <ButtonToGo label="I FORGOT TO CHECK IN" path="/main" />
-            <ButtonToGo label="TIME TO RESET" path={`/ShowingIntegrity/${id}`}   />
-
+                <ButtonToGo label="I FORGOT TO CHECK IN" path="/main" />
+                <ButtonToGo label="TIME TO RESET" path={`/ShowingIntegrity/${id}`} />
+            </div>
+            </div>
         </div>
     )
 }
